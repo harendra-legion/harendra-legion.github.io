dee14fcf9bd83c5c4ef87f2a9ef3efdd4ef3d550
harendra-legion <harendra@legion.is>
2026-02-04 22:52:22 +0530

    Update hb-loader.js

--- hb-loader.js
+++ hb-loader.js
@@ -1,6 +1,26 @@
+// (function () {
+//   var hbScript = document.createElement("script");
+//   hbScript.async = true;
+//   hbScript.src = "https://my.hellobar.com/4093d06fd4de0bf422a37110bed1ede31e999683.js";
+//   document.head.appendChild(hbScript);
+// })();
+
 (function () {
-  var hbScript = document.createElement("script");
-  hbScript.async = true;
-  hbScript.src = "https://my.hellobar.com/4093d06fd4de0bf422a37110bed1ede31e999683.js";
-  document.head.appendChild(hbScript);
+
+  var script = document.createElement("script");
+  script.async = true;
+
+  var params = new URLSearchParams(window.location.search);
+  var env = params.get("env");
+
+  if (env === "staging") {
+    script.src = "https://my.hellobar.com/7b66a980171a6371913aa19deeb3847f6992c96f.js";
+    console.log("HelloBar: STAGING loaded");
+  } else {
+    script.src = "https://my.hellobar.com/4093d06fd4de0bf422a37110bed1ede31e999683.js";
+    console.log("HelloBar: PRODUCTION loaded");
+  }
+
+  document.head.appendChild(script);
+
 })();
